bad4296a3b758492c0ab19d7fa8a97d5bf97886f
David Lin <davll.xc@gmail.com>
2017-10-30 16:41:54 +0800

    add x86

--- build_android.sh
+++ build_android.sh
@@ -11,3 +11,19 @@ cmake -DCMAKE_TOOLCHAIN_FILE=cmake/android.toolchain.cmake \
       -DgRPC_USE_BENCHMARK=OFF                       \
       ..
 make grpc_csharp_ext
+cd ..
+
+mkdir -p .build-android-x86
+cd .build-android-x86
+cmake -DCMAKE_TOOLCHAIN_FILE=cmake/android.toolchain.cmake \
+      -DANDROID_NDK=$ANDROID_NDK_ROOT                \
+      -DCMAKE_BUILD_TYPE=Release                     \
+      -DANDROID_ABI="x86"                            \
+      -DANDROID_NATIVE_API_LEVEL=23                  \
+      -DBENCHMARK_ENABLE_TESTING=NO                  \
+      -DgRPC_BUILD_TESTS=NO                          \
+      -Dprotobuf_BUILD_EXAMPLES=NO                   \
+      -DgRPC_USE_BENCHMARK=OFF                       \
+      ..
+make grpc_csharp_ext
+cd ..
