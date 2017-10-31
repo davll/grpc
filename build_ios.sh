9da1df5fe276ba68ba603b2ed9389c616cbd6e57
David Lin <davll.xc@gmail.com>
2017-10-31 10:43:20 +0800

    fix ios build

--- build_ios.sh
+++ build_ios.sh
@@ -1,5 +1,5 @@
-mkdir -p .build-ios-armv7
-cd .build-ios-armv7
+mkdir -p .build-ios/armv7
+cd .build-ios/armv7
 cmake -DIOS_PLATFORM=OS \
       -DENABLE_BITCODE=FALSE \
       -DgRPC_USE_BENCHMARK=OFF \
@@ -11,12 +11,12 @@ cmake -DIOS_PLATFORM=OS \
       -DgRPC_BUILD_TESTS=NO                          \
       -Dprotobuf_BUILD_EXAMPLES=NO                   \
       -DCMAKE_BUILD_TYPE=Release                     \
-      ..
+      ../..
 make grpc_csharp_ext
-cd ..
+cd ../..
 
-mkdir -p .build-ios-armv7s
-cd .build-ios-armv7s
+mkdir -p .build-ios/armv7s
+cd .build-ios/armv7s
 cmake -DIOS_PLATFORM=OS \
       -DENABLE_BITCODE=FALSE \
       -DgRPC_USE_BENCHMARK=OFF \
@@ -28,12 +28,12 @@ cmake -DIOS_PLATFORM=OS \
       -DgRPC_BUILD_TESTS=NO                          \
       -Dprotobuf_BUILD_EXAMPLES=NO                   \
       -DCMAKE_BUILD_TYPE=Release                     \
-      ..
+      ../..
 make grpc_csharp_ext
-cd ..
+cd ../..
 
-mkdir -p .build-ios-arm64
-cd .build-ios-arm64
+mkdir -p .build-ios/arm64
+cd .build-ios/arm64
 cmake -DIOS_PLATFORM=OS \
       -DENABLE_BITCODE=FALSE \
       -DgRPC_USE_BENCHMARK=OFF \
@@ -45,8 +45,39 @@ cmake -DIOS_PLATFORM=OS \
       -DgRPC_BUILD_TESTS=NO                          \
       -Dprotobuf_BUILD_EXAMPLES=NO                   \
       -DCMAKE_BUILD_TYPE=Release                     \
-      ..
+      -DIOS=TRUE                       \
+      ../..
 make grpc_csharp_ext
-cd ..
+cd ../..
 
-#lipo .build-ios-armv7/lib
+mkdir -p .build-ios
+cd .build-ios
+
+lipo armv7/libgpr.a armv7s/libgpr.a arm64/libgpr.a \
+     -create -output libgpr.a
+
+lipo armv7/libgrpc.a armv7s/libgrpc.a arm64/libgrpc.a \
+     -create -output libgrpc.a
+
+lipo armv7/libgrpc_csharp_ext.a armv7s/libgrpc_csharp_ext.a arm64/libgrpc_csharp_ext.a \
+     -create -output libgrpc_csharp_ext.a
+
+lipo armv7/third_party/boringssl/crypto/libcrypto.a \
+     armv7s/third_party/boringssl/crypto/libcrypto.a \
+     arm64/third_party/boringssl/crypto/libcrypto.a \
+     -create -output libcrypto.a
+
+lipo armv7/third_party/boringssl/ssl/libssl.a \
+     armv7s/third_party/boringssl/ssl/libssl.a \
+     arm64/third_party/boringssl/ssl/libssl.a \
+     -create -output libssl.a
+
+lipo armv7/third_party/cares/cares/lib/libcares.a \
+     armv7s/third_party/cares/cares/lib/libcares.a \
+     arm64/third_party/cares/cares/lib/libcares.a \
+     -create -output libcares.a
+
+lipo armv7/third_party/zlib/libz.a \
+     armv7s/third_party/zlib/libz.a \
+     arm64/third_party/zlib/libz.a \
+     -create -output libz.a
