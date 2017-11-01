740aa44c4d6a2604de6836535a63d26b623cee2d
David Lin <davll.xc@gmail.com>
2017-11-01 17:50:51 +0800

    Build for iOS platform

--- build-ios.sh
+++ build-ios.sh
@@ -1,56 +1,53 @@
 mkdir -p build-ios/armv7
 cd build-ios/armv7
-cmake -DIOS_PLATFORM=OS \
-      -DENABLE_BITCODE=FALSE \
-      -DgRPC_USE_BENCHMARK=OFF \
-      -DCMAKE_OSX_SYSROOT=iphoneos \
-      -DCMAKE_OSX_ARCHITECTURES=armv7 \
-      -DPERL_EXECUTABLE=/usr/bin/perl \
-      -DGO_EXECUTABLE=/usr/local/bin/go \
-      -DBENCHMARK_ENABLE_TESTING=NO                  \
-      -DgRPC_BUILD_TESTS=NO                          \
-      -Dprotobuf_BUILD_EXAMPLES=NO                   \
-      -DCMAKE_BUILD_TYPE=Release                     \
-      -DIOS=TRUE                       \
-      -DgRPC_ZLIB_PROVIDER=package \
+cmake -DCMAKE_TOOLCHAIN_FILE=cmake/ios.toolchain.cmake \
+      -DIOS_DEPLOYMENT_TARGET=7.0                      \
+      -DIOS_PLATFORM=OS                                \
+      -DIOS_ARCH=armv7                                 \
+      -DCMAKE_SYSTEM_PROCESSOR=arm                     \
+      -DPERL_EXECUTABLE=/usr/bin/perl                  \
+      -DGO_EXECUTABLE=/usr/local/bin/go                \
+      -DgRPC_ZLIB_PROVIDER=package                     \
+      -DgRPC_BENCHMARK_PROVIDER=module                 \
+      -DgRPC_BUILD_TESTS=NO                            \
+      -Dprotobuf_BUILD_EXAMPLES=NO                     \
+      -DCMAKE_BUILD_TYPE=Release                       \
       ../..
 make grpc_csharp_ext
 cd ../..
 
 mkdir -p build-ios/armv7s
 cd build-ios/armv7s
-cmake -DIOS_PLATFORM=OS \
-      -DENABLE_BITCODE=FALSE \
-      -DgRPC_USE_BENCHMARK=OFF \
-      -DCMAKE_OSX_SYSROOT=iphoneos \
-      -DCMAKE_OSX_ARCHITECTURES=armv7s \
-      -DPERL_EXECUTABLE=/usr/bin/perl \
-      -DGO_EXECUTABLE=/usr/local/bin/go \
-      -DBENCHMARK_ENABLE_TESTING=NO                  \
-      -DgRPC_BUILD_TESTS=NO                          \
-      -Dprotobuf_BUILD_EXAMPLES=NO                   \
-      -DCMAKE_BUILD_TYPE=Release                     \
-      -DgRPC_ZLIB_PROVIDER=package \
-      -DIOS=TRUE                       \
+cmake -DCMAKE_TOOLCHAIN_FILE=cmake/ios.toolchain.cmake \
+      -DIOS_DEPLOYMENT_TARGET=7.0                      \
+      -DIOS_PLATFORM=OS                                \
+      -DIOS_ARCH=armv7s                                \
+      -DCMAKE_SYSTEM_PROCESSOR=arm                     \
+      -DPERL_EXECUTABLE=/usr/bin/perl                  \
+      -DGO_EXECUTABLE=/usr/local/bin/go                \
+      -DgRPC_ZLIB_PROVIDER=package                     \
+      -DgRPC_BENCHMARK_PROVIDER=module                 \
+      -DgRPC_BUILD_TESTS=NO                            \
+      -Dprotobuf_BUILD_EXAMPLES=NO                     \
+      -DCMAKE_BUILD_TYPE=Release                       \
       ../..
 make grpc_csharp_ext
 cd ../..
 
 mkdir -p build-ios/arm64
 cd build-ios/arm64
-cmake -DIOS_PLATFORM=OS \
-      -DENABLE_BITCODE=FALSE \
-      -DgRPC_USE_BENCHMARK=OFF \
-      -DCMAKE_OSX_SYSROOT=iphoneos \
-      -DCMAKE_OSX_ARCHITECTURES=arm64 \
-      -DPERL_EXECUTABLE=/usr/bin/perl \
-      -DGO_EXECUTABLE=/usr/local/bin/go \
-      -DBENCHMARK_ENABLE_TESTING=NO                  \
-      -DgRPC_BUILD_TESTS=NO                          \
-      -Dprotobuf_BUILD_EXAMPLES=NO                   \
-      -DCMAKE_BUILD_TYPE=Release                     \
-      -DIOS=TRUE                       \
-      -DgRPC_ZLIB_PROVIDER=package \
+cmake -DCMAKE_TOOLCHAIN_FILE=cmake/ios.toolchain.cmake \
+      -DIOS_DEPLOYMENT_TARGET=7.0                      \
+      -DIOS_PLATFORM=OS                                \
+      -DIOS_ARCH=arm64                                 \
+      -DCMAKE_SYSTEM_PROCESSOR=aarch64                  \
+      -DPERL_EXECUTABLE=/usr/bin/perl                  \
+      -DGO_EXECUTABLE=/usr/local/bin/go                \
+      -DgRPC_ZLIB_PROVIDER=package                     \
+      -DgRPC_BENCHMARK_PROVIDER=module                 \
+      -DgRPC_BUILD_TESTS=NO                            \
+      -Dprotobuf_BUILD_EXAMPLES=NO                     \
+      -DCMAKE_BUILD_TYPE=Release                       \
       ../..
 make grpc_csharp_ext
 cd ../..
